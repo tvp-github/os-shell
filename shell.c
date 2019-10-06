cafe6bef32b3c417fe361ec5cc3b9ee6255cbf0f
TaVinhPhuc <tavinhphuc.ctg@gmail.com>
2019-10-06 05:38:03 -0700

    Added 3rd and 4th feature

--- shell.c
+++ shell.c
@@ -1,25 +1,15 @@
-/* ----------------------------------------------------------------- */
-/* PROGRAM  shell.c                                                  */
-/*    This program reads in an input line, parses the input line     */
-/* into tokens, and use execvp() to execute the command.             */
-/* ----------------------------------------------------------------- */
-
 #include  <stdio.h>
 #include  <string.h>
 #include  <stdlib.h>
 #include  <unistd.h>
 #include  <sys/wait.h>
+#include  <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #define MAX_LINE 80 /* The maximum length command */
-/* ----------------------------------------------------------------- */
-/* FUNCTION  parse:                                                  */
-/*    This function takes an input line and parse it into tokens.    */
-/* It first replaces all white spaces with zeros until it hits a     */
-/* non-white space character which indicates the beginning of an     */
-/* argument.  It saves the address to argv[], and then skips all     */
-/* non-white spaces which constitute the argument.                   */
-/* ----------------------------------------------------------------- */
-
-void  parse(char *line, char **argv, char** output, char** input)
+/* Convert command to argv list */
+/* Get input, output, pipe mark position from the command */ 
+void  parse(char *line, char **argv, char** output, char** input, char*** pipePos)
 {
      int get = 0;
      char* curLine;
@@ -40,13 +30,15 @@ void  parse(char *line, char **argv, char** output, char** input)
           if (strcmp(curLine, ">")==0)/* Has redirected output          */
           {
                get = 1;
-               *argv--;
+               *argv--; 
+               /*Remove output filename from then argv */
           }
           else 
           if (strcmp(curLine, "<")==0)/* Has redirected input           */
           {
                get = 2;
                *argv--;
+               /*Remove input filename from then argv */
           }
           else if(get)             /* Get this argv to redirected    */
           {
@@ -58,21 +50,13 @@ void  parse(char *line, char **argv, char** output, char** input)
                *argv--;
           }
           
-          /* if use for redirected then remove this argv from list*/               
-
+          if(strcmp(curLine,"|")==0)
+               *pipePos = argv - 1; 
      }
-     *argv = '\0';                 /* mark the end of argument list  */
+     *argv = '\0';                /* The end of the argv is \0*/
 }
-
-/* ----------------------------------------------------------------- */
-/* FUNCTION execute:                                                 */
-/*    This function receives a commend line argument list with the   */
-/* first one being a file name followed by its arguments.  Then,     */
-/* this function forks a child process to execute the command using  */
-/* system call execvp().                                             */
-/* ----------------------------------------------------------------- */
-     
-void  execute(char **argv)
+/* Execute the comand */     
+void  execute(char **argv,char* output, char* input, char** pipePos)
 {
      pid_t  pid;
      int    status;
@@ -82,35 +66,89 @@ void  execute(char **argv)
           exit(1);
      }
      else if (pid == 0) {          /* for the child process:         */
-          if (execvp(*argv, argv) < 0) {     /* execute the command  */
-               printf("*** ERROR: exec failed\n");
-               exit(1);
+          //Redirected output
+          if(output)
+          {
+               int fd = open(output, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IXUSR);
+               dup2(fd,1);
+          }
+          else
+          //Redirected input
+          if(input){
+               int fd = open(input, O_RDONLY);
+               dup2(fd,0);
+          }
+          //If there is not a pipe mark then execute the command
+          if (!pipePos)
+               {
+                    if (execvp(*argv, argv) < 0) {     /* execute the command  */
+                         printf("*** ERROR: exec failed\n");
+                         exit(1);
+                    }
+               }
+          else
+          {
+               int fd[2];
+               if(pipe(fd)<0)
+                    {
+                         printf("Pipe filed\n");
+                         exit(1);
+                    }
+               /* Devide argv list into 2 argv list for each child */
+               *pipePos = '\0';
+               char** argvPipe = pipePos +1;
+               pid_t childPid = fork();
+               if (childPid == 0)
+               {
+                    //Child-child process
+                    close(fd[0]);
+                    //Redirected output to fd[0]
+                    dup2(fd[1], 1);
+                    if (execvp(*argv, argv) < 0) {     /* execute the command  in child's child*/
+                         printf("*** ERROR: exec failed\n");
+                         exit(1);
+                    }
+               } 
+               else
+               {
+                    //Child process
+                    int childStatus;
+                    //Redirected input to fd[1]
+                    close(fd[1]);
+                    while (wait(&status) != childPid)       /* wait for completion  */
+                         ;
+                    dup2(fd[0],0);
+                    if (execvp(*argvPipe, argvPipe) < 0) {     /* execute the command in child */
+                         printf("*** ERROR: exec failed\n");
+                         exit(1);
+                    }
+
+               }
           }
      }
-     else {                                  /* for the parent:      */
-          while (wait(&status) != pid)       /* wait for completion  */
+     else {                                  /* parent process      */
+          while (wait(&status) != pid)       /* wait for child completion  */
                ;
      }
 }
-
-/* ----------------------------------------------------------------- */
-/*                  The main program starts here                     */
-/* ----------------------------------------------------------------- */
      
 void  main(void)
 {
-     char  line[MAX_LINE];             /* the input line                 */
-     char  *argv[MAX_LINE/2 +1];              /* the command line argument      */
+     char  line[MAX_LINE];             /* the input command                 */
+     char  *argv[MAX_LINE/2 +1];              /* the command -> argument      */
      char backupLine[MAX_LINE];
      int backup = 0;
      while (1) { 
           /* repeat until done ....         */
+          char** pipePos;
           char* input;
           char* output;
-          input = output = NULL;                  
-          printf("osh> ");     /*   display a prompt             */
-          fgets(line, MAX_LINE, stdin);       /*   read in the command line     */
+          input = output = NULL;
+          pipePos = NULL;                  
+          printf("osh> ");     /*   display prompt             */
+          fgets(line, MAX_LINE, stdin);       /*   read the command    */
           line[strlen(line)-1] = 0; /* remove LF char */
+          if(strlen(line)==0) continue;/*Empty command*/
           /*Check history */
           if(strcmp(line,"!!")==0) /*use previous command*/
             if (backup)
@@ -122,12 +160,9 @@ void  main(void)
                 strcpy(backupLine,line); /* if not then backup current command */
                 backup = 1;
             }
-          parse(line, argv, &output, &input);       /*   parse the line               */
-          if(input) printf("%s\n", input);
-          if(output) printf("%s\n", output);
-
-          if (strcmp(argv[0], "exit") == 0)  /* is it an "exit"?     */
-               exit(0);            /*   exit if it is                */
-          execute(argv);           /* otherwise, execute the command */
+          parse(line, argv, &output, &input, &pipePos);       /*   parse the line to argv               */
+          if (strcmp(argv[0], "exit") == 0)  /* if user type exit then exit     */
+               exit(0);            
+          execute(argv,output,input,pipePos);           /* execute command */
      }
 }
